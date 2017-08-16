1a973a96da634948e89d16fec6ac557ffc84b341
lars-alfonse <lars.alfonse@gmail.com>
2017-08-16 14:31:30 -0500

    added more Add function tests including FormatException and retaining object values

--- CustomListUnitTest/UnitTest1.cs
+++ CustomListUnitTest/UnitTest1.cs
@@ -9,9 +9,9 @@ namespace CustomListUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void CheckIfAdds()
+        public void AddAddsAString()
         {
-            CustomListUnitTest<string> list = new CustomListUnitTest<string>;
+            CustomList<string> list = new CustomList<string>;
             string expectedValue = "test";
 
             list.Add("test");
@@ -20,9 +20,9 @@ namespace CustomListUnitTest
             Assert.AreEqual(actualValue, expectedValue);
         }
         [TestMethod]
-        public void CheckIfAddRetainsOldValue()
+        public void AddRetainsOldValues()
         {
-            CustomListUnitTest<string> list = new CustomListUnitTest<string>;
+            CustomList<string> list = new CustomList<string>;
             string expectedValue = "test";
 
             list.Add("test");
@@ -30,6 +30,41 @@ namespace CustomListUnitTest
             string actualValue = list[0];
 
             Assert.AreEqual(actualValue, expectedValue);
+        }
+        [TestMethod]
+        public void AddRetainsOrder()
+        {
+            CustomList<string> list = new CustomList<string>;
+            string expectedValue = "test";
+
+            list.Add("!test");
+            list.Add("test");
+            list.Add("!test");
+            string actualValue = list[1];
+
+            Assert.AreEqual(actualValue, expectedValue);
+        }
+        [TestMethod]
+        [ExpectedException(typeof(FormatException))]
+        public void StringListDoesntAllowAddInteger()
+        {
+            CustomList<string> list = new CustomList<string>;
+
+            list.Add("!test");
+            list.Add(1);;
+        }
+        [TestMethod]
+        public void AddKeepsObjectMemberVariables()
+        {
+            CustomList<TestObject> list = new CustomList<TestObject>;
+            TestObject testObject = new TestObject();
+            testObject.name = "test name";
+            string expectedValue = "test name";
+
+            list.Add(testObject);
+            string actualValue = list[0].name;
+
+            Assert.AreEqual(expectedValue, actualValue);
         }
     }
 }
