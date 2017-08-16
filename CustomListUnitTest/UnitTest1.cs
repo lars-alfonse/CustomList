8f4ea3eee1be77a7976f4dde50b539c349fcf57a
lars-alfonse <lars.alfonse@gmail.com>
2017-08-16 13:49:23 -0500

    added test project

---
+++ CustomListUnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using CustomList;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace CustomListUnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
